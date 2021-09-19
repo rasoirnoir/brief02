6f7453be648facdc5a609cefa74b6e4e9d90d271
William NORIS <w.noris@protonmail.com>
2021-09-19 17:26:16 +0200

    différents test sur les aiguilles

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
-
+const currentHour = new Date();
 
 //Stocker l'heure , minute , seconde  dans des varaiables
-
+let hour = currentHour.getHours();
+let minute = currentHour.getMinutes();
+let second = currentHour.getSeconds();
+console.log(hour, minute, second);
 
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
